45d5f2e10caf45818245e2f6351057d29a5b9265
Keshav <keshavtomar999@gmail.com>
2022-06-30 03:06:02 +0530

    storing users in DB without validation

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Keshav')
-})
+app.use(express.json())
+
+// Avaliable Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
